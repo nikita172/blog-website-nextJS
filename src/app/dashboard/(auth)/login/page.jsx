2b79fc1c14b9158b9b091c56f86d13c3f64434c1
nikita-rawat <nikitarwt789@gmail.com>
2023-07-04 22:09:34 +0530

    credential authorization using credentialsProvider

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -3,9 +3,35 @@ import React from 'react'
 import styles from "./page.module.css"
 import { signIn } from 'next-auth/react'
 const Login = () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    signIn("credentials", { email, password })
+
+
+  }
   return (
     <div className={styles.container}>
-      <button onClick={() => signIn("google")}>Login with google</button>
+      <h1 className={styles.title}>Create an Account</h1>
+      <h2 className={styles.subtitle}>Please sign up to see the dashboard.</h2>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Email"
+          required
+          className={styles.input}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          required
+          className={styles.input}
+        />
+        <button className={styles.button}>Login</button>
+      </form>
+      <button onClick={() => signIn("google")}>login with google</button>
     </div>
   )
 }
