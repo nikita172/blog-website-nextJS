c7ab1b16be3d87a559ab45f20ae62c9e0ff98c87
nikita-rawat <nikitarwt789@gmail.com>
2023-07-04 18:39:46 +0530

    google authentication

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,8 @@ import './globals.css'
 import { Inter, Roboto, Poppins } from 'next/font/google'
 import Footer from '@/components/footer/Footer'
 import { ThemeProvider } from '@/context/ThemeContext'
+import { SessionProvider } from "next-auth/react"
+import AuthProvider from '@/components/AuthProvider/AuthProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,11 +18,14 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <ThemeProvider>
-          <div className="container">
-            <Navbar />
-            {children}
-            <Footer />
-          </div>
+          <AuthProvider>
+
+            <div className="container">
+              <Navbar />
+              {children}
+              <Footer />
+            </div>
+          </AuthProvider>
         </ThemeProvider>
       </body>
     </html>
