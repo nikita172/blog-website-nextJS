77a7e2b2ba0e375e0ca53bf7b4889a8e181c8a26
nikita-rawat <nikitarwt789@gmail.com>
2023-07-05 10:57:47 +0530

    created dashboard & its endpoint for adding post

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -2,15 +2,33 @@ import { NextResponse } from "next/server"
 import connect from "@/utils/db.js"
 import Post from "@/models/Post";
 export const GET = async (request) => {
+  const url = new URL(request.url);
+  const username = url.searchParams.get("username")
   //fetch
   try {
     await connect();
-    const posts = await Post.find();
+    const posts = await Post.find(username && { username });
     return new NextResponse(JSON.stringify(posts), { status: 200 })
 
 
   } catch (err) {
     return new NextResponse("Database error", { status: 500 })
+
+  }
+
+}
+
+export const POST = async (request) => {
+  const body = await request.json();
+  const newPost = new Post(body);
+  try {
+    await connect();
+    await newPost.save();
+    return new NextResponse("Post has been created", { status: 201 })
+
+
+  } catch (err) {
+    return new NextResponse("Something went wrong!", { status: 500 })
 
   }
 
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -4,6 +4,7 @@ import styles from "./page.module.css"
 import useSWR from 'swr'
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
+import Image from 'next/image'
 
 const Dashboard = () => {
   // const [data, setData] = useState([])
@@ -29,20 +30,61 @@ const Dashboard = () => {
   const session = useSession()
   const router = useRouter()
   const fetcher = (...args) => fetch(...args).then(res => res.json());
-  const { data, error, isLoading } = useSWR("https://jsonplaceholder.typicode.com/posts", fetcher);
+  const { data, error, mutate, isLoading } = useSWR(`/api/posts?username=${session?.data?.user.name}`, fetcher);
+  console.log(data)
+
   if (session.status === "loading") {
     return <p>Loading...</p>
   }
   if (session.status === "unauthenticated") {
     router?.push("/dashboard/login")
   }
-
-
-
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const title = e.target[0].value;
+    const desc = e.target[1].value;
+    const img = e.target[2].value;
+    const content = e.target[3].value;
+    try {
+      await fetch("/api/posts", {
+        method: "POST",
+        body: JSON.stringify({
+          title, desc, img, content, username: session.data.user.name,
+        })
+      })
+      mutate();
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
   if (session.status === "authenticated") {
     return (
-      <div>Dashboard</div>
+      <div className={styles.container}>
+        <div className={styles.posts}>
+          {isLoading ? "loading..." : data?.map(post => (
+            <div className={styles.post} key={post._id}>
+              <div className={styles.imgContainer}>
+                <Image src={post.img} alt="" width={200} height={100} />
+              </div>
+              <h2 className={styles.postTitle}>{post.title}</h2>
+              <span className={styles.delete}>X</span>
+            </div>
+          ))}
+
+        </div>
+
+        <form className={styles.new} onSubmit={handleSubmit}>
+          <h1>Add new Post</h1>
+          <input type="text" placeholder='Title' className={styles.input} />
+          <input type="text" placeholder='Desc' className={styles.input} />
+          <input type="text" placeholder='Image ' className={styles.input} />
+          <textarea placeholder='content' className={StyleSheetList.textArea} cols="30" rows="10" ></textarea>
+          <button className={styles.button}>Send</button>
+
+        </form>
+
+      </div>
     )
   }
 
